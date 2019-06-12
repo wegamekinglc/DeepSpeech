a6974d3dc2e4030408aaed0820072e89eb75ea7e
wegamekinglc <wegamekinglc@hotmail.com>
2019-06-12 23:57:04 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,14 @@ if [ $? != 0 ]; then
     rm libsndfile-1.0.28.tar.gz
 fi
 
+cp packages/pa_stable_v190600_20161030.tgz .
+tar -zxvf pa_stable_v190600_20161030.tgz
+cd portaudio
+./configure > /dev/null && make > /dev/null && make install > /dev/null
+cd ..
+rm -rf portaudio
+rm pa_stable_v190600_20161030.tgz
+
 # install decoders
 python -c "import pkg_resources; pkg_resources.require(\"swig_decoders==1.1\")"
 if [ $? != 0 ]; then
@@ -29,6 +37,9 @@ if [ $? != 0 ]; then
     sh setup.sh
     cd - > /dev/null
 fi
+
+
+
 
 
 echo "Install all dependencies successfully."
